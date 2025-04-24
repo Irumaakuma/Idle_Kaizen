6e9bd847dfa260f7276dbd54f5d6ba3f8506a6fa
irumaakuma <spriggy.pro@gmail.com>
2025-04-24 05:30:51 +0200

    fix: redirect to GitHub Pages after Discord login

--- task.js
+++ task.js
@@ -1,5 +1,5 @@
 class Task {
-  constructor(id, name, baseXpPerTick = 1) {
+  constructor(id, name, baseXpPerTick = 10) {
     this.id = id;
     this.name = name;
     this.level = 0;
@@ -29,7 +29,7 @@ class Task {
 }
 
 class Skill {
-  constructor({ id, name, baseXpGain = 10, baseEffect = 0.05, group = "autre", unlocked = true, getBonusDamage, getBonusDodge, getTripleHitChance, getIgnoreDefense, maxLevel }) {
+  constructor({ id, name, baseXpGain = 20, baseEffect = 0.05, group = "autre", unlocked = true, getBonusDamage, getBonusDodge, getTripleHitChance, getIgnoreDefense, maxLevel }) {
     this.id = id;
     this.name = name;
     this.level = 1;
